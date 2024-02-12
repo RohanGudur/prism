8553dd06c3a9d3a0ebfc69d23a4f0195c8dcb952
Azaken1248 <suvkrm@gmail.com>
2024-02-12 18:14:44 +0530

    Adding necessary files and submission

--- CalanderAPI/tempCodeRunnerFile.py
+++ CalanderAPI/tempCodeRunnerFile.py
@@ -1,39 +1,3 @@
-import datetime
-import os
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-
-SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
-
-def write_file(data):
-  with open('events.txt','a+') as file:
-    file.write(data)
-
-'''
-RUN ONLY IN COLAB 
-
-from google.colab import drive
-from google.colab import files
-
-# Mount Google Drive to Colab
-drive.mount('/content/gdrive')
-
-# Define the local file path
-local_file_path = 'path/to/your/local/file.txt'
-
-# Define the destination folder and file name on Google Drive
-drive_folder = '/content/gdrive/My Drive/'
-drive_file_name = 'uploaded_file.txt'
-
-# Copy the local file to Google Drive
-!cp "{local_file_path}" "{drive_folder}{drive_file_name}"
-'''
-
 def fetchScheduleData():
 
   creds = None
@@ -45,7 +9,7 @@ def fetchScheduleData():
       creds.refresh(Request())
     else:
       flow = InstalledAppFlow.from_client_secrets_file(
-          "C:/Users/Rohit Sinha/Desktop/SamsungPrism/CalanderAPI/Config/credentials.json", SCOPES
+          "./Config/credentials.json", SCOPES
       )
       creds = flow.run_local_server(port=0)
     with open("token.json", "w") as token:
